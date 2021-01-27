ba465a2f7a3d6408572087dde6137a27cb0207c1
Tekzards <tekzards@gmail.com>
2021-01-28 00:19:08 +0530

    Initial Setup

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Honest Shopy!!!</Text>
+      <Button title="Click" icon={{name : 'code'}}></Button>
       <StatusBar style="auto" />
     </View>
   );
