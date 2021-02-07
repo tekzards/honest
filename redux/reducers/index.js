d02dcb2adac42859aaa5597fe40d025b49d45df2
unknown <tekzards@gmail.com>
2021-02-07 02:53:51 -0800

    changes

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { combineReducers } from 'redux'
 import { user } from './user'
 
@@ -6,13 +5,4 @@ const Reducers = combineReducers({
     userState: user
 })
 
-=======
-import { combineReducers } from 'redux'
-import { user } from './user'
-
-const Reducers = combineReducers({
-    userState: user
-})
-
->>>>>>> cf655e0c3e810776603fb322fe5d6c8bed233b52
 export default Reducers
--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const initialState = {
     currentUser: null
 }
@@ -8,15 +7,4 @@ export const user = (state = initialState, action) => {
         ...state,
         currentUser: action.currentUser
     }
-=======
-const initialState = {
-    currentUser: null
-}
-
-export const user = (state = initialState, action) => {
-    return {
-        ...state,
-        currentUser: action.currentUser
-    }
->>>>>>> cf655e0c3e810776603fb322fe5d6c8bed233b52
 }
